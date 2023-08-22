92e90c6001a8fd9812be4ef51bd57b3e48068f6a
Mauritz Marais <mauritz303@gmail.com>
2023-08-22 14:09:59 +0200

    added functions to switch theme and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
 
+
 import './App.css';
-// import { useState } from 'react';
+import { useState } from 'react';
 import SearchContainer from './components/searchContainer';
+import Navbar from './components/navbar';
 
 
 function App() {
 
+  const [theme, setTheme] = useState('light');
+  const [font, setFont] = useState('serif');
+
+  // switch theme function
+  function switchTheme() {
+    setTheme('light' === theme ? 'dark' : 'light')
+  }
+
+  // switch font function
+  function switchFont(event) {
+    setFont(event.target.value)
+  }
+
   return (
     <>
-      <h1>Dictionary App</h1>
-      <SearchContainer />
+      <div className='app' data-theme={theme} data-font={font}>
+        <Navbar switchTheme={switchTheme} switchFont={switchFont} />
+        <h1>Dictionary App</h1>
+        <SearchContainer />
+      </div>
     </>
   )
 }
